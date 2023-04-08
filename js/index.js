6d3a66cee2746acd6093f064636c73d0f07b9dfa
Cookie that I made <52488720+ejh118@users.noreply.github.com>
2023-04-08 19:23:51 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
 
 
+  //스크롤 수치
+  $(window).on("scroll", function(){
+    var scroll_top=$(this).scrollTop();
+    $(".top").text(scroll_top);
+  });
+
+
 //트리거 클릭시 GNB toggle, 트리거 변경
   $(".trigger").click(function(){
     $(this).toggleClass("active");
     $(".gnbBox").toggleClass("active");
+    $("#logo").toggleClass("active");
+  });
+
+  $(window).scroll(function(){
+
+    let scollpos=$(this).scrollTop();
+    chek=true;
+    if(scollpos > 800){
+      $("header").css({"background-color":"#fff","border-bottom":"1px solid #eee"});
+      $(".trigger span").css({"background-color":"#000"})
+      $("#logo").attr("src","image/logo_black.png")
+      chek=false;
+    }else{
+      $("header").css({"background-color":"transparent","border":"none"});
+      $(".trigger span").css({"background-color":"#fff"});
+
+      $("#logo").attr("src","image/logo.png")
+    }
+
   });
 });
