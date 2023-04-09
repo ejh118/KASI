664273490f36c8a15d0a72dc440bcb9cf1804df3
Cookie that I made <52488720+ejh118@users.noreply.github.com>
2023-04-09 18:54:55 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 
   $(".trigger").click(function(){
     $(this).toggleClass("active");
+    $(".gnbBar").toggleClass("active");
   });
-  
+
+
   $(".closePopUp").click(function(){
     $(".popUp").slideUp();
   });
