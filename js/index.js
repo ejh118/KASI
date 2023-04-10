a3861b68c48a787ac7191987fefb41ef386aa537
Cookie that I made <52488720+ejh118@users.noreply.github.com>
2023-04-10 16:33:16 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,67 +1,93 @@
 $(document).ready(function(){
 
-
-  //스크롤 수치
-  $(window).on("scroll", function(){
-    var scroll_top=$(this).scrollTop();
-    $(".top").text(scroll_top);
+    //스크롤 수치
+    $(window).on("scroll", function(){
+      var scroll_top=$(this).scrollTop();
+      $(".top").text(scroll_top);
+    });
+  
+  $(".trigger").click(function(){
+    $(this).toggleClass("active");
+    $(".gnbBar").toggleClass("active");
   });
 
 
-//트리거 클릭시 GNB toggle, 트리거 변경
-  $(".trigger").click(function(){
-    $(this).toggleClass("active");
-    $(".gnbBox").toggleClass("active");
+  $(".closePopUp").click(function(){
+    $(".popUp").slideUp();
+  });
+
+
+  /* 스크롤 애니메이션 조작 */
+
+  $(window).scroll(function(){
+    let scollpos=$(this).scrollTop();
+    chek=true;
+    if(scollpos > 400){
+      $(".article1_one h2").css({"top":"280px","opacity":"1"});
+      $(".article1_two img").css({"top":"-170px","opacity":"1"});
+      chek=false;
+    }else{
+      $(".article1_one h2").css({"top":"300px","opacity":"0"});
+      $(".article1_two img").css({"top":"-190px","opacity":"0"});
+    }
   });
 
   $(window).scroll(function(){
+    let scollpos=$(this).scrollTop();
+    chek=true;
+    if(scollpos > 500){
+      $(".article1_two p").css({"opacity":"1"});
+      chek=false;
+    }else{
+      $(".article1_two p").css({"opacity":"0"});
+    }
+  });
 
+  $(window).scroll(function(){
     let scollpos=$(this).scrollTop();
     chek=true;
     if(scollpos > 700){
-      $("header").css({"background-color":"#fff","border-bottom":"1px solid #eee"});
-      $(".trigger span").css({"background-color":"#000"})
-      $("#logo").attr("src","image/logo_black.png")
+      $(".article2_in h2").css({"opacity":"1"});
       chek=false;
     }else{
-      $("header").css({"background-color":"rgba(19, 19, 19, 0.74)","border":"none"});
-      $(".trigger span").css({"background-color":"#fff"});
-
-      $("#logo").attr("src","image/logo.png")
+      $(".article2_in h2").css({"opacity":"0"});
     }
-    
   });
-
   $(window).scroll(function(){
-
     let scollpos=$(this).scrollTop();
     chek=true;
     if(scollpos > 800){
-      $("body").css({"background-color":"#000"});
-      $(".fixed_Content").css({"position":"fixed","opacity":"1"});
-      
+      $(".article2_in p").css({"opacity":"1"});
       chek=false;
     }else{
-      $("body").css({"background-color":"#fff"});
-      $(".fixed_Content").css({"opacity":"0"});
+      $(".article2_in p").css({"opacity":"0"});
     }
-    
   });
   $(window).scroll(function(){
-
     let scollpos=$(this).scrollTop();
     chek=true;
-    if(scollpos > 2000){
-      $(".fixed_Content img").attr("src","image/mission-02.jpg");
-      $(".fixed_Content h2").text("지속가능한 성장을 위한 햄심기술 개발에 집중합니다.");
-
+    if(scollpos > 1300){
+      $(".article3_one h2").css({"opacity":"1"});
       chek=false;
     }else{
-      $(".fixed_Content img").attr("src","image/mission-01.jpg");
-      $(".fixed_Content h2").text("우리의 솔루션으로 지구보호와 세계평화를 추구합니다.");
+      $(".article3_one h2").css({"opacity":"0"});
     }
-    
   });
+
+  $(window).scroll(function(){
+    let scollpos=$(this).scrollTop();
+    chek=true;
+    if(scollpos > 1500){
+      $(".article3_two img").css({"opacity":"1","top":"-120px"});
+      $(".article3_two_Text").css({"opacity":"1"})
+      chek=false;
+    }else{
+      $(".article3_two img").css({"opacity":"0","top":"-100px"});
+      $(".article3_two_Text").css({"opacity":"0"})
+    }
+  });
+
+
 
 
 
